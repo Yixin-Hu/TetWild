11f2d6df11425ce8f883a0c7b2f75655e167f63f
YIxin-Hu <yh1998@nyu.edu>
2018-05-25 12:32:44 -0400

    add license

--- BSPSubdivision.cpp
+++ BSPSubdivision.cpp
@@ -1,3 +1,10 @@
+// This file is part of TetWild, a software for generating tetrahedral meshes.
+// 
+// Copyright (C) 2018 Yixin Hu <yixin.hu@nyu.edu>
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License 
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+// obtain one at http://mozilla.org/MPL/2.0/.
 //
 // Created by Yixin Hu on 4/3/17.
 //
