bf94b746ebb3196c974fc860728f76c79e36a69b
Trevor Laughlin <trevor@ntopology.com>
2018-07-05 18:53:51 -0400

    Initial support for appveyor and travis-ci

---
+++ ci/conda/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+mkdir build
+cd build
+
+cmake -G "Ninja" \
+    -DCMAKE_BUILD_TYPE="Release" \
+	-DCMAKE_INSTALL_PREFIX:FILEPATH=$HOME/.local/ \
+	-DBUILD_TESTING=OFF \
+	..
+
+ninja install
