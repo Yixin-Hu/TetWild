62e85512625eae1414926c34ae7faa603f40573b
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-09-06 17:29:33 -0400

    Download Boost via Hunter.

---
+++ cmake/TetWildDependencies.cmake
@@ -0,0 +1,39 @@
+################################################################################
+# CMake download helpers
+################################################################################
+
+# download external dependencies
+include(TetWildDownloadExternal)
+
+################################################################################
+# Required dependencies
+################################################################################
+
+# Boost
+if(TETWILD_WITH_HUNTER)
+	hunter_add_package(Boost COMPONENTS thread system)
+endif()
+
+# spdlog
+if(NOT TARGET spdlog::spdlog)
+	tetwild_download_spdlog()
+	add_subdirectory(${TETWILD_EXTERNAL}/spdlog)
+endif()
+
+# libigl
+if(NOT TARGET igl::core)
+	tetwild_download_libigl()
+	find_package(LIBIGL REQUIRED)
+endif()
+
+# geogram
+if(NOT TARGET geogram)
+	tetwild_download_geogram()
+	include(geogram)
+endif()
+
+# pymesh loaders
+add_subdirectory(${TETWILD_EXTERNAL}/pymesh)
+
+# CL11
+add_subdirectory(${TETWILD_EXTERNAL}/CLI)
