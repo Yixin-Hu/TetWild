Yixin-Hu/TetWild

commit f88e653baa8b7bac2e14f28166af2de4df8e7bf0

Low hanging fruits optimizations.

src/tetwild/Common.cpp
@@ -42,13 +42,27 @@ void pausee(){
 }
 
 bool isHaveCommonEle(const std::unordered_set<int>& v1, const std::unordered_set<int>& v2) {
-    for (auto it = v1.begin(); it != v1.end(); it++)
-        if(std::find(v2.begin(), v2.end(), *it)!=v2.end())
-            return true;
+    if (v1.size() < v2.size()) {
+        for (int x : v1) {
+            if (v2.count(x)) {
+				return true;
+			}
+        }
+    } else {
+        for (int x : v2) {
+            if (v1.count(x)) {
+				return true;
+			}
+        }
+    }
+    // for (auto it = v1.begin(); it != v1.end(); it++)
+    //     if(std::find(v2.begin(), v2.end(), *it)!=v2.end())
+    //         return true;
     return false;
 }
 
 void setIntersection(const std::unordered_set<int>& s1, const std::unordered_set<int>& s2, std::unordered_set<int>& s) {
+#if 0
     std::unordered_set<int> s_tmp;
     std::vector<int> v1, v2;
     v1.reserve(s1.size());
@@ -61,6 +75,16 @@ void setIntersection(const std::unordered_set<int>& s1, const std::unordered_set
     std::sort(v2.begin(), v2.end());
     std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::inserter(s_tmp, s_tmp.end()));
     s = s_tmp;
+#else
+	if (s2.size() < s1.size()) { setIntersection(s2, s1, s); return; }
+	s.clear();
+	s.reserve(std::min(s1.size(), s2.size()));
+	for (int x : s1) {
+		if (s2.count(x)) {
+			s.insert(x);
+		}
+	}
+#endif
 
 //    s.clear();
 //    s.reserve(std::min(s1.size(), s2.size()));
@@ -87,6 +111,7 @@ void setIntersection(const std::unordered_set<int>& s1, const std::unordered_set
 //        } else
 //            s.push_back(ele);
 //    }
+#if 0
     std::vector<int> v1, v2;
     v1.reserve(s1.size());
     for(auto it=s1.begin();it!=s1.end();it++)
@@ -97,6 +122,16 @@ void setIntersection(const std::unordered_set<int>& s1, const std::unordered_set
     std::sort(v1.begin(), v1.end());
     std::sort(v2.begin(), v2.end());
     std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(s));
+#else
+	if (s2.size() < s1.size()) { setIntersection(s2, s1, s); return; }
+	s.clear();
+	s.reserve(std::min(s1.size(), s2.size()));
+	for (int x : s1) {
+		if (s2.count(x)) {
+			s.push_back(x);
+		}
+	}
+#endif
 }
 
 

src/tetwild/LocalOperations.cpp
@@ -969,13 +969,14 @@ bool LocalOperations::isEdgeOnBoundary(int v1_id, int v2_id) {
 
     int cnt = 0;
     for (int t_id: tet_vertices[v1_id].conn_tets) {
-        std::vector<int> opp_js;
+		std::array<int, 4> opp_js;
+		int cnt = 0;
         for (int j = 0; j < 4; j++) {
             if (tets[t_id][j] == v1_id || tets[t_id][j] == v2_id)
                 continue;
-            opp_js.push_back(j);
+            opp_js[cnt++] = j;
         }
-        if (opp_js.size() == 2) {
+        if (cnt == 2) {
             if (is_surface_fs[t_id][opp_js[0]] != State::state().NOT_SURFACE)
                 cnt++;
             if (is_surface_fs[t_id][opp_js[1]] != State::state().NOT_SURFACE)

commit 7d63ce766caba90885153fef5396fba7bcf4bb95

Fix regression introduced by f88e653

src/tetwild/Common.cpp
@@ -45,22 +45,20 @@ void pausee(){
 }
 
 bool isHaveCommonEle(const std::unordered_set<int>& v1, const std::unordered_set<int>& v2) {
-    if (v1.size() < v2.size()) {
-        for (int x : v1) {
-            if (v2.count(x)) {
-				return true;
-			}
-        }
-    } else {
-        for (int x : v2) {
-            if (v1.count(x)) {
-				return true;
-			}
+#if 0
+    for (auto it = v1.begin(); it != v1.end(); it++)
+        if(std::find(v2.begin(), v2.end(), *it)!=v2.end())
+            return true;
+#else
+    if (v2.size() < v1.size()) {
+        return isHaveCommonEle(v2, v1);
+    }
+    for (int x : v1) {
+        if (v2.count(x)) {
+            return true;
         }
     }
-    // for (auto it = v1.begin(); it != v1.end(); it++)
-    //     if(std::find(v2.begin(), v2.end(), *it)!=v2.end())
-    //         return true;
+#endif
     return false;
 }
 
@@ -79,14 +77,14 @@ void setIntersection(const std::unordered_set<int>& s1, const std::unordered_set
     std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::inserter(s_tmp, s_tmp.end()));
     s = s_tmp;
 #else
-	if (s2.size() < s1.size()) { setIntersection(s2, s1, s); return; }
-	s.clear();
-	s.reserve(std::min(s1.size(), s2.size()));
-	for (int x : s1) {
-		if (s2.count(x)) {
-			s.insert(x);
-		}
-	}
+    if (s2.size() < s1.size()) { setIntersection(s2, s1, s); return; }
+    s.clear();
+    s.reserve(std::min(s1.size(), s2.size()));
+    for (int x : s1) {
+        if (s2.count(x)) {
+            s.insert(x);
+        }
+    }
 #endif
 
 //    s.clear();
@@ -126,14 +124,15 @@ void setIntersection(const std::unordered_set<int>& s1, const std::unordered_set
     std::sort(v2.begin(), v2.end());
     std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(s));
 #else
-	if (s2.size() < s1.size()) { setIntersection(s2, s1, s); return; }
-	s.clear();
-	s.reserve(std::min(s1.size(), s2.size()));
-	for (int x : s1) {
-		if (s2.count(x)) {
-			s.push_back(x);
-		}
-	}
+    if (s2.size() < s1.size()) { setIntersection(s2, s1, s); return; }
+    s.clear();
+    s.reserve(std::min(s1.size(), s2.size()));
+    for (int x : s1) {
+        if (s2.count(x)) {
+            s.push_back(x);
+        }
+    }
+    std::sort(s.begin(), s.end());
 #endif
 }
 

src/tetwild/LocalOperations.cpp
@@ -969,14 +969,14 @@ bool LocalOperations::isEdgeOnBoundary(int v1_id, int v2_id) {
 
     int cnt = 0;
     for (int t_id: tet_vertices[v1_id].conn_tets) {
-		std::array<int, 4> opp_js;
-		int cnt = 0;
+        std::array<int, 4> opp_js;
+        int ii = 0;
         for (int j = 0; j < 4; j++) {
             if (tets[t_id][j] == v1_id || tets[t_id][j] == v2_id)
                 continue;
-            opp_js[cnt++] = j;
+            opp_js[ii++] = j;
         }
-        if (cnt == 2) {
+        if (ii == 2) {
             if (is_surface_fs[t_id][opp_js[0]] != State::state().NOT_SURFACE)
                 cnt++;
             if (is_surface_fs[t_id][opp_js[1]] != State::state().NOT_SURFACE)