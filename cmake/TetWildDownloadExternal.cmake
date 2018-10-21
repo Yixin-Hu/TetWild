c70dedf7108bf7f5e899044c26f2d9def5512f42
Yixin Hu <yh1998@nyu.edu>
2018-10-20 22:59:30 -0400

    update to new libigl version

--- cmake/TetWildDownloadExternal.cmake
+++ cmake/TetWildDownloadExternal.cmake
@@ -16,8 +16,8 @@ endfunction()
 ## libigl
 function(tetwild_download_libigl)
     tetwild_download_project(libigl
-        GIT_REPOSITORY https://github.com/jdumas/libigl.git
-        GIT_TAG        5cfc34dd1680cfe5a6e545ed9f89ae5cf2d644b7
+        GIT_REPOSITORY https://github.com/libigl/libigl
+        GIT_TAG        ed363da1f624e7a2f802f6b04973be8ea63f0511
     )
 endfunction()
 
